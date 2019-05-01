bbb1e5fb212c79495aab68083b89ca8b54d5f0d3
Eduardo Henrique <edu.hitman01.eh@gmail.com>
2019-04-30 22:22:55 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/eduardo/cursomc/domain/Categoria.java
+++ src/main/java/com/eduardo/cursomc/domain/Categoria.java
@@ -2,9 +2,20 @@ package com.eduardo.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable{
     private static final long serialVersionUID = 1L;
-	private Integer id;
+	
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)   // definição da estratégia de geração de numero no banco
+    private Integer id;
 	private String nome;
 	
 	public Categoria() {
--- src/main/java/com/eduardo/cursomc/resouces/CategoriaResource.java
+++ src/main/java/com/eduardo/cursomc/resouces/CategoriaResource.java
@@ -24,6 +24,4 @@ public class CategoriaResource {
 		
 		return lista;
 	}
-	
-	
 }
