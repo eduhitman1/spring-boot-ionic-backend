40dd665cbb0c3624fc56214d26b9d61fc326908c
Eduardo Henrique <edu.hitman01.eh@gmail.com>
2019-05-05 20:05:45 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/eduardo/cursomc/services/CategoriaService.java
+++ src/main/java/com/eduardo/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.eduardo.cursomc.domain.Categoria;
+import com.eduardo.cursomc.dto.CategoriaDTO;
 import com.eduardo.cursomc.repositories.CategoriaRepository;
 import com.eduardo.cursomc.services.exceptions.DataIntegrityException;
 import com.eduardo.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,5 +58,13 @@ public class CategoriaService {
     	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
         return repo.findAll(pageRequest);
     }
+    
+    
+    public Categoria fromDTO(CategoriaDTO objDto) {
+    	return new Categoria(objDto.getId(), objDto.getNome());
+    }
+    
+    
+    
     
 }
