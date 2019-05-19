bf13c4ecc097d8bc8995cc92f3fbf5dd55bf6c37
Eduardo Henrique <edu.hitman01.eh@gmail.com>
2019-05-19 18:24:44 -0300

    Implementando ToString de Pedido

--- src/main/java/com/eduardo/cursomc/domain/Pedido.java
+++ src/main/java/com/eduardo/cursomc/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.eduardo.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -161,8 +164,26 @@ public class Pedido implements Serializable{
 	}
 
 
-
-	
-	
-	
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR")); // FORMATAÇÃO DE FLOAT
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss"); // FORMATAÇÃO DE DATA
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido número: ");
+        builder.append(getId());
+        builder.append(", Instante: ");
+        builder.append(sdf.format(getInstante())); // FORMATAÇÃO DE DATA
+        builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+        builder.append("\nDetalhes:\n");
+        
+        for(ItemPedido ip : getItens()) {
+        	builder.append(ip.toString());
+        }
+        builder.append("Valor total: ");
+       builder.append(nf.format(getValorTotal())); //FORMATADOR DE FLOAT 
+        return builder.toString();
+	}
 }
