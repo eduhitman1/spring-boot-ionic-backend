cdb9075009da71bc8ae4f7300d7881c79d4aa13e
Eduardo Henrique <edu.hitman01.eh@gmail.com>
2019-06-02 19:35:35 -0300

    Enviando imagem via endpoint

--- src/main/java/com/eduardo/cursomc/resouces/ClienteResource.java
+++ src/main/java/com/eduardo/cursomc/resouces/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eduardo.cursomc.domain.Cliente;
@@ -24,80 +25,78 @@ import com.eduardo.cursomc.dto.ClienteNewDTO;
 import com.eduardo.cursomc.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/clientes")
+@RequestMapping(value = "/clientes")
 public class ClienteResource {
 
 	@Autowired
 	private ClienteService service;
 
-	
-	//METODO DE INSERÇÃO NO *ENDPOINT
-		@RequestMapping(method=RequestMethod.POST)
-		public ResponseEntity<Void>  insert(@Valid @RequestBody  ClienteNewDTO objDto){
-			Cliente obj = service.fromDTO(objDto);
-			obj = service.insert(obj);
-		    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		    
+	// METODO DE INSERÇÃO NO *ENDPOINT
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-		}
-	
-		//METODO DE UPDATE    NO *ENDPOINT
-	@RequestMapping(value= "/{id}",method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+	}
+
+	// METODO DE UPDATE NO *ENDPOINT
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
 		Cliente obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
-	//METODO DE DELETE    NO *ENDPOINT
-	@PreAuthorize("hasAnyRole('ADMIN')")   // SOMENTE ADMINISTRADOR
-	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)           // PASSANDO O ID DA CATEGORIA
-	public ResponseEntity<Void> delete(@PathVariable Integer id) {        // pathvariable é para identificar o id value
-	service.delete(id);
-	return ResponseEntity.noContent().build();
-	}
-	
-	//METODO DE LISTAGEM  NO *ENDPOINT
-	@PreAuthorize("hasAnyRole('ADMIN')")    // SOMENTE ADMINISTRADOR
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<ClienteDTO>> findAll() { 
-		List<Cliente> list = service.findAll();     
-		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDto);       	 
-	}
-	
-	//METODO DE LISTAGEM POR PAGINAÇÃO NO *ENDPOINT
-	@RequestMapping(value="/page",method=RequestMethod.GET)
-	public ResponseEntity<Page<ClienteDTO>> findPage(
-		@RequestParam(value="page",defaultValue="0")    Integer page, 
-		@RequestParam(value="linesPerPage",defaultValue="24")	Integer linesPerPage, 
-		@RequestParam(value="orderBy",defaultValue="nome")	String orderBy,             //ordenação por nome 
-		@RequestParam(value="direction",defaultValue="ASC")	String direction){         //ordenação por crescente
-		Page<Cliente> list = service.findPage(page,linesPerPage,orderBy,direction);     
-		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
-		return ResponseEntity.ok().body(listDto);       	 
-	}
-	
-	
-      //COD ESTAVA ACIMA DOS METODO CRUD	
-	  @RequestMapping(value="/{id}",method=RequestMethod.GET)           // PASSANDO O ID DA CATEGORIA
-	  public ResponseEntity<Cliente> find(@PathVariable Integer id) {        // pathvariable é para identificar o id value 
-		
-		Cliente obj = service.find(id);                            // BUSCANDO O METÓDO NA CLASS categoriaService 
-		return ResponseEntity.ok().body(obj);                      // CORPO DO OBJETO	 
-	                                                               //	Cliente cat1 = new Cliente(1, "Informatica");
-	                                                               //	Cliente cat2 = new Cliente(2, "Escritório");
-	                                                               //	List<Cliente> lista = new ArrayList<>();
-	                                                               //	lista.add(cat1);
-	                                                               //	lista.add(cat2);
-	                                                               //	return lista;
-		
 
-		
-		
-		
-		
-		
+	// METODO DE DELETE NO *ENDPOINT
+	@PreAuthorize("hasAnyRole('ADMIN')") // SOMENTE ADMINISTRADOR
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE) // PASSANDO O ID DA CATEGORIA
+	public ResponseEntity<Void> delete(@PathVariable Integer id) { // pathvariable é para identificar o id value
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
+
+	// METODO DE LISTAGEM NO *ENDPOINT
+	@PreAuthorize("hasAnyRole('ADMIN')") // SOMENTE ADMINISTRADOR
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	// METODO DE LISTAGEM POR PAGINAÇÃO NO *ENDPOINT
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, // ordenação por nome
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) { // ordenação por crescente
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	// COD ESTAVA ACIMA DOS METODO CRUD
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET) // PASSANDO O ID DA CATEGORIA
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) { // pathvariable é para identificar o id value
+
+		Cliente obj = service.find(id); // BUSCANDO O METÓDO NA CLASS categoriaService
+		return ResponseEntity.ok().body(obj); // CORPO DO OBJETO
+												// Cliente cat1 = new Cliente(1, "Informatica");
+												// Cliente cat2 = new Cliente(2, "Escritório");
+												// List<Cliente> lista = new ArrayList<>();
+												// lista.add(cat1);
+												// lista.add(cat2);
+												// return lista;
+
+	}
+
+	// END POINT DE PICTURE
+	@RequestMapping(value="/picture", method=RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file) {
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- src/main/java/com/eduardo/cursomc/services/ClienteService.java
+++ src/main/java/com/eduardo/cursomc/services/ClienteService.java
@@ -1,4 +1,6 @@
 package com.eduardo.cursomc.services;
+
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.eduardo.cursomc.domain.Cidade;
 import com.eduardo.cursomc.domain.Cliente;
@@ -27,100 +30,97 @@ import com.eduardo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-    @Autowired                           // VAI SER INSTANCIA AUTOMATICAMENTO PELO SPRING
-    private ClienteRepository repo;    // DEPENDECIA DE OBJETO
-	
-   @Autowired 
-   private EnderecoRepository enderecoRepository;
-    
-   @Autowired
-   private BCryptPasswordEncoder pe;
-   
-    
-    public Cliente find(Integer id){
-    	
-    	UserSS user = UserService.authenticated();
-    	if(user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
-    		throw new AuthorizationException("Acesso negado");   
-    	}
-    	
-    	
-        Optional<Cliente> obj = repo.findById(id);
-        return obj.orElseThrow(() -> new ObjectNotFoundException(
-                "Objeto não encontrado! Id: "+id+",Tipo: "+ Cliente.class.getName()));
-  } 
-    
-    
-  //METÓDO DE INSERÇÃO NO *ENDPOINT
-    @Transactional 
-    public Cliente insert(Cliente obj){
-    obj.setId(null);
-    obj = repo.save(obj);
-    enderecoRepository.saveAll(obj.getEnderecos());
-    return obj;
-    }
-    
-    //METÓDO DE UPDATE  NO *ENDPOINT
-    public Cliente update(Cliente obj) {
-    Cliente newObj = find(obj.getId());         // ESTÁ PUCHANDO O METÓDO ACIMA FIND
-    updateData(newObj, obj);
-    return repo.save(newObj);
-    //return repo.save(obj);     // RESPONSE UPDATE OBJ
-    }
-     
-    //METÓDO DE DELETE NO *ENDPOINT
-    public void delete(Integer id) {
-    	find(id);
-    	try{
-    	repo.deleteById(id);
-    	} catch(DataIntegrityViolationException e) {
-    	  throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");	
-    	}
-    }
-    
-    //METÓDO DE LISTAGEM NO *ENDPOINT
-    public List<Cliente> findAll(){
-    	return repo.findAll();
-    }
-    //METÓDO DE LISTAGEM POR PAGINAÇÃO, NÃO PUXANDO TODA LISTAGEM
-    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
-    	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
-        return repo.findAll(pageRequest);
-    }
-    
-    private void updateData(Cliente newObj , Cliente obj) {
-    	newObj.setNome(obj.getNome());
-    	newObj.setEmail(obj.getEmail());
-    }
-    
-    // CLIENTEDTO
-    public Cliente fromDTO(ClienteDTO objDto) {
-       return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null,null);	
-    }
- // CLIENTENEWDTO
-    public Cliente fromDTO(ClienteNewDTO objDto) {
-        Cliente cli = new Cliente(null , objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));	
-     
-        Cidade cid = new Cidade(objDto.getCidadeId(),null,null);
-        Endereco end = new  Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
-       cli.getEnderecos().add(end);
-       cli.getTelefones().add(objDto.getTelefone1());
-       if(objDto.getTelefone2()!= null) {
-    	   cli.getTelefones().add(objDto.getTelefone2());
-       }
-       if(objDto.getTelefone3()!= null) {
-    	   cli.getTelefones().add(objDto.getTelefone3());
-       }
-    
-       return cli;
-    
-    }
-    
+	@Autowired // VAI SER INSTANCIA AUTOMATICAMENTO PELO SPRING
+	private ClienteRepository repo; // DEPENDECIA DE OBJETO
 
-    
-   // VERSÃO 1.5..   
-   //   public Cliente buscar(Integer id) {
-   //   Cliente obj = repo.findOne(id);
-   //    return obj;
-   //   }
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder pe;
+
+	@Autowired
+	private S3Service s3Service;
+
+	public Cliente find(Integer id) {
+
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ",Tipo: " + Cliente.class.getName()));
+	}
+
+	// METÓDO DE INSERÇÃO NO *ENDPOINT
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
+	// METÓDO DE UPDATE NO *ENDPOINT
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId()); // ESTÁ PUCHANDO O METÓDO ACIMA FIND
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		// return repo.save(obj); // RESPONSE UPDATE OBJ
+	}
+
+	// METÓDO DE DELETE NO *ENDPOINT
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
+		}
+	}
+
+	// METÓDO DE LISTAGEM NO *ENDPOINT
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	// METÓDO DE LISTAGEM POR PAGINAÇÃO, NÃO PUXANDO TODA LISTAGEM
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	// CLIENTEDTO
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
+	}
+
+	// CLIENTENEWDTO
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
+	}
 }
